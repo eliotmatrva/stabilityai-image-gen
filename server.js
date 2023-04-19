37ac36452503d1aa4de5450b2b23be963f747eca
Matt Elliott <eliotmat@gmail.com>
2023-04-18 23:01:36 -0400

    work in progress request for generateImage()

--- server.js
+++ server.js
@@ -10,7 +10,33 @@ let endpoints = {
 
 console.log(url + endpoints.imageGen);
 
+let engineId = 'stable-diffusion-xl-beta-v2-2-2';
 
+let reqHeaders = new Headers();
+    reqHeaders.append('Authorization', `Bearer ${process.env.stabilityApiKey}`);
+    reqHeaders.append('Content-Type', 'application/json');
+
+async function generateImage(prompt) {
+    let endpoint = `https://api.stability.ai/v1/generation/${engineId}/text-to-image/`
+    let body = {
+        "cfg_scale": 7,
+            "clip_guidance_preset": "FAST_BLUE",
+            "height": 512,
+            "width": 512,
+            "sampler": "K_DPM_2_ANCESTRAL",
+            "samples": 1,
+            "steps": 75,
+            "text_prompts": [
+                {
+                "text": "A lighthouse on a cliff",
+                "weight": 1
+                }
+            ]
+    }
+    let options = {
+        method:
+    }
+}
 
 (async () => {
     let myHeaders = new Headers();
