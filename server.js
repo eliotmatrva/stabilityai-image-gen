b99ba702d0012400b0a9b9daf16a0c2745d1d616
Matt Elliott <eliotmat@gmail.com>
2023-04-25 22:10:13 -0400

    added express and first api post request for responding with latest image file name.  Need to actually update to send the file.

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
 const fs = require('fs');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
 let url = 'https://api.stability.ai';
 let endpoints = {
@@ -44,7 +52,7 @@ let reqHeaders = new Headers();
     reqHeaders.append('Content-Type', 'application/json');
 
 // discord links for generate image:  https://discord.com/channels/1002292111942635562/1042896447311454361/1096559685974368298
-let imagePrompt = "geometrically patterned stained glass window where the glass pieces have mayan symbols";
+let imagePrompt = "a real photograph of a funny fat man playing at the beach";
 
 function writeIndexFile(indexJson, prompt, timeStamp){
     let promptFileIndex = JSON.parse(fs.readFileSync(indexJson));
@@ -60,7 +68,7 @@ async function generateImage(engineId, prompt) {
         "height": 512,
         "width": 512,
         "samples": 1,
-        "steps": 30,
+        "steps": 40,
         "text_prompts": [{
             "text": prompt,
             "weight": 1
@@ -93,5 +101,26 @@ async function getEngines(){
     console.log(data);
 }
 
-generateImage('stable-diffusion-xl-beta-v2-2-2', imagePrompt);
+// generateImage('stable-diffusion-xl-beta-v2-2-2', imagePrompt);
 
+app.get('/api/getLatestImage', async (req, res) => {
+    let imageList = JSON.parse(fs.readFileSync('./prompt-and-file-index.json', 'utf8'));
+    console.log(typeof(imageList));
+    
+    function compareFileNames(a,b) {
+        if (a.imageFileName > b.imageFileName) {
+            return -1;
+        } else if (a.imageFileName < b.imageFileName){
+            return 1;
+        } else {
+            return 0;
+        }
+    };
+    let sorted = imageList.sort(compareFileNames);
+    console.log(JSON.stringify(sorted[0]));
+    res.send(JSON.stringify(sorted[0]));
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
+})
